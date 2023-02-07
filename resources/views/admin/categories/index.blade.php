8d7b11ee7d317b54100808280d4aa97e4676e168
Kenta Bilotta <kentabilotta20@gmail.com>
2023-02-07 21:51:07 +0100

    first commit

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @if (session('success_delete'))
+            <div class="alert alert-warning" role="alert">
+                Category "{{ session('success_delete')->name }}" deleted successfully
+            </div>
+        @endif
+
+        <h1>Categories</h1>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr>
+                        <th scope="row">{{ $category->id }}</th>
+                        <td>{{ $category->slug }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>
+                            <a href="{{ route('admin.categories.show', ['category' => $category]) }}" class="btn btn-primary">More</a>
+                        </td>
+                        <td>
+                            <a href="{{ route('admin.categories.edit', ['category' => $category]) }}" class="btn btn-warning">Edit</a>
+                        </td>
+                        {{-- <td>
+                            <button class="btn btn-danger btn-delete-me" data-id="{{ $category->id }}">Delete</button>
+                        </td> --}}
+                        <td>
+                            <form action="{{ route('admin.categories.destroy', ['category' => $category]) }}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-danger btn-delete-me">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        {{ $categories->links() }}
+    </div>
+@endsection
